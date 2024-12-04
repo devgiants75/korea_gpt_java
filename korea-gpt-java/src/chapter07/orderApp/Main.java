261f265735e9fb9f299977e48a484a5308e24675
devgiants75 <devgiants75@naver.com>
2024-12-04 11:31:05 +0900

    20241203 abstract, interface

--- korea-gpt-java/src/chapter07/orderApp/Main.java
+++ korea-gpt-java/src/chapter07/orderApp/Main.java
@@ -17,5 +17,10 @@ public class Main {
 
         System.out.println(order1); // Order Detail[ Laptop * Quantity: 1 >> 5750.0 ]
         System.out.println(order2); // Order Detail[ Apple * Quantity: 10 >> 60000.0 ]
+
+        // toString()
+        // : '클래스명@해당객체의주소값'이 문자열로 출력
+        // >> 객체를 문자열로 표현할 때 사용
+        // >> 모든 클래스의 최상위 클래스인 Object 클래스에 정의
     }
 }
