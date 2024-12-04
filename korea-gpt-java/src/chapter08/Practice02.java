1ddfd01104118a2118d8a81ea6f029a1a5fb954c
devgiants75 <devgiants75@naver.com>
2024-12-04 17:21:05 +0900

    20241204 exception

--- korea-gpt-java/src/chapter08/Practice02.java
+++ korea-gpt-java/src/chapter08/Practice02.java
@@ -90,6 +90,32 @@ public class Practice02 {
             }
         }
 
+        // cf) A instanceof B 연산자
+        // : "A 객체가 B 클래스(종류)와 같은지 확인해줘" 도구
+        // >> 확인의 결괏값을 boolean으로 반환
+        class 동물 {}
+        class 고양이 extends 동물 {}
+        class 강아지 extends 동물 {}
+
+        동물 애니멀 = new 동물();
+        동물 나비 = new 고양이();
+        동물 멍멍이 = new 강아지();
+
+        if (애니멀 instanceof 고양이) {
+            System.out.println("실행 되지 않음! 부모객체를 자식객체에 자동으로 담을 수 없음");
+        }
+
+        // 고양이 고양이로바꿈 = (고양이) 애니멀;
+        // : 형 변환을 시킬 수 없는 상황
+
+        if (나비 instanceof 고양이 && 나비 instanceof 동물) { // 성공
+            System.out.println("성공");
+        }
+
+        if (멍멍이 instanceof 고양이) {
+            System.out.println("성공?");
+        }
+
         if (mango instanceof Fruit) {
             Fruit mangoAsFruit = (Fruit) mango;
             mangoAsFruit.describe(); // 과일의 색상은golden이며, 모양은 타원형입니다.
