c363384dcfe235c582d8a2d87caf06a0f4c0e7cd
devgiants75 <devgiants75@naver.com>
2024-12-05 17:44:52 +0900

    20241205 library system

---
+++ korea-gpt-java/src/chapter10/practice/LibrarySystem.java
@@ -0,0 +1,10 @@
+package chapter10.practice;
+
+import java.util.Scanner;
+
+public class LibrarySystem {
+    public static void main(String[] args) {
+        LibraryManager manager = new LibraryManager();
+        Scanner scanner = new Scanner(System.in);
+    }
+}
