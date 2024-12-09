885cc27cc458723115e59833e0148966ac9de32e
devgiants75 <devgiants75@naver.com>
2024-12-09 16:27:28 +0900

    20241209 MVC

---
+++ korea-gpt-java/src/chapter12/controller/StudentController.java
@@ -0,0 +1,21 @@
+package chapter12.controller;
+
+import chapter12.model.Student;
+
+import java.util.List;
+
+public class StudentController {
+    public void addStudent(String name, int age, String id) {
+    }
+
+
+    public List<Student> getAllStudents() {
+        return null;
+    }
+
+    public boolean updateStudent(String id, String newName, int newAge) {
+    }
+
+    public boolean removeStudent(String id) {
+    }
+}
