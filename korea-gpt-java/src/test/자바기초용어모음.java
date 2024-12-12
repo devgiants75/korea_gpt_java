21043fabb6cb4822ae461cd5674f96ce875d65a5
devgiants75 <devgiants75@naver.com>
2024-12-12 14:08:51 +0900

    20241212

---
+++ korea-gpt-java/src/test/자바기초용어모음.java
@@ -0,0 +1,49 @@
+package test;
+
+public class 자바기초용어모음 {
+    // 접근 제한자
+
+
+
+
+    // static
+
+
+
+
+    // final
+
+
+
+
+    // super VS this
+
+
+
+
+    // 오버로딩 VS 오버라이딩
+
+
+
+
+    // abstract
+
+
+
+
+    // interface VS abstract class
+
+
+
+
+    // extends VS implements
+
+
+
+
+    // try-catch-finally
+
+
+
+
+}
