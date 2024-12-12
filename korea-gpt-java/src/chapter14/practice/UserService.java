9519887e2a6a7b80c613c81c9fe71170e88f898e
devgiants75 <devgiants75@naver.com>
2024-12-12 12:13:12 +0900

    20241211 review

--- korea-gpt-java/src/chapter14/practice/UserService.java
+++ korea-gpt-java/src/chapter14/practice/UserService.java
@@ -1,9 +1,12 @@
 package chapter14.practice;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserService {
-    public User signUp(UserSignUpRequestDto dto) {
+    public static User signUp(UserSignUpRequestDto dto) {
         String password = dto.getPassword();
         String passwordCheck = dto.getPasswordCheck();
 
@@ -29,6 +32,31 @@ public class UserService {
     }
 
     public static void main(String[] args) {
+        UserSignUpRequestDto dto1 = new UserSignUpRequestDto(
+                "123", "1234", "1234"
+                , "이승아", "qwe", "01011112222", "개구리"
+        );
 
+        User user1 = signUp(dto1);
+        System.out.println(user1.getSignUpDate()); // Thu Dec 12 12:06:11 KST 2024
+
+        List<User> userList = null;
+        List<UserSignUpRequestDto> dtos = new ArrayList<>();
+        dtos.add(new UserSignUpRequestDto("123", "1234", "1234"
+                , "이승아1", "qwe", "01011112222", "개구리"));
+        dtos.add(new UserSignUpRequestDto("123", "1234", "1234"
+                , "이승아2", "qwe", "01011112222", "개구리"));
+        dtos.add(new UserSignUpRequestDto("123", "1234", "1234"
+                , "이승아3", "qwe", "01011112222", "개구리"));
+
+        userList = dtos.stream()
+                .map(dto -> signUp(dto))
+                .collect(Collectors.toList());
+
+        userList = dtos.stream()
+                .map(UserService::signUp)
+                .collect(Collectors.toList());
+
+        userList.forEach(System.out::println);
     }
 }
